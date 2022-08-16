0ccb127297f5308e823b176a3ad072e7281bc61c
ramazantufekci <ramazan.tufekci@outlook.com>
2022-08-16 09:00:56 +0300

    kdv oran değişikliği

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ public class Main {
         Scanner giris = new Scanner(System.in);
         System.out.print("Ücret Tutarını Giriniz :" );
         tutar = giris.nextDouble();
+
+        kdvOran = tutar<1000?0.18:0.08;
         kdvTutar = tutar * kdvOran;
         kdvliTutar = tutar + kdvTutar;
+        
         System.out.println("Tutar: "+tutar);
         System.out.println("Kdv Oranı : "+kdvOran);
         System.out.println("Kdv Tutarı : "+kdvTutar);
